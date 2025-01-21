0fc842a788e3cf40480bcb394c0b7b1464900425
miguel garcia <miguel.gareva@gmail.com>
2025-01-21 14:46:15 +0100

    first 5 queries made

--- queries/queries_restaurantes.js
+++ queries/queries_restaurantes.js
@@ -1,8 +1,8 @@
 /*1*/Sprint2.restaurantes.find({});
-/**/Sprint2.restaurantes.
-/**/Sprint2.restaurantes.
-/**/Sprint2.restaurantes.
-/**/Sprint2.restaurantes.
+/*2*/Sprint2.restaurantes.find({},{restaurant_id: 1, name: 1, borough: 1, cuisine: 1});
+/*3*/Sprint2.restaurantes.find({},{restaurant_id: 1, name: 1, borough: 1, cuisine: 1, _id:0});
+/*4*/Sprint2.restaurantes.find({},{restaurant_id: 1, name: 1, borough: 1, "adress.zipcode": 1, _id:0});
+/**/Sprint2.restaurantes.find({borough: "Bronx"});
 /**/Sprint2.restaurantes.
 /**/Sprint2.restaurantes.
 /**/Sprint2.restaurantes.
